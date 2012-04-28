7c876d4c6ea97b289a8c0f0519f5b05ebfccc513
Chethan <chethan@ubuntu.ubuntu-domain>
2012-04-28 17:34:46 +0530

    created db tables for challenge and solution

--- coderapp/models.py
+++ coderapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Challenge(models.Model):
+	title = models.CharField(max_length=250) #a problem title
+	description_url = models.CharField(max_length=200) #should it be more than this?
+
+class Solution(models.Model):
+	challenge = models.ForeignKey(Challenge) #a solution belongs to a challenge
+	#user = model.ForeignKey(User) #a solution has a user
+	location = models.CharField(max_length=200) #a location for the solution. A directory probably
+	score = models.IntegerField() #overall rank? we need more metrics	
